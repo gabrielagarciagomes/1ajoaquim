507693058dfb0acb3a51efa7cef86b11844c4c74
gabrielagarciagomes <gabriela.garcia.gomes@escola.pr.gov.br>
2024-09-26 12:04:30 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+// fantasia, aventura, drama
+
+// a viajem de chihiro, LIVRE, fantasia, aventura
+// guardiõs da galáxia,12, fantaia, aventura
+
+// a aventuras de pi, 10 , drama, fantasia, aventura
+// paddington, LIVRE, fantasia, aventura
+// ladrões de bicicletas, 12, drama
+// o menino que descobriu o vento, 14, drama
+
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+
+
+
+
+
+
+
+
+
+
+
